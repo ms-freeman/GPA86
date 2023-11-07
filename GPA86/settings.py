8688834d24fbd01ad593c6f3fca74123c782e553
ms-freeman <alex.29_46@live.fr>
2023-11-07 12:18:37 +0100

    2eme commit

--- GPA86/settings.py
+++ GPA86/settings.py
@@ -69,6 +69,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'GPA86.wsgi.application'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Database
